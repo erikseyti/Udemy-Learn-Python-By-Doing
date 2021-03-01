eb99fff813afb77883cb1ba8271a306b6e56fc6f
Erik Seyti <erikseyti@gmail.com>
2021-02-28 21:31:31 -0300

    Criado exemplo de utilizacao de String em Python

---
+++ Section 1 - Intro To Python/python_strings.py
@@ -0,0 +1,19 @@
+my_string = "Hello, it's Word!"
+print(my_string)
+another_with_quotes = 'He said "You are amazing!" Yesterday.'
+print(another_with_quotes)
+# exemplo de utilizar um caracter de escape para poder utilizar o mesmo tipo de "" na string.
+another_example_quotes = "He said \"You are amazing!\" Yesterday."
+print(another_example_quotes)
+# exemplo de string com multiplas linhas.
+multiline = """Hello World!
+
+My name is Erik. Welcome to the program! ;)
+"""
+print(multiline)
+name = "Erik"
+greeting = "Hello, "+ name
+print(greeting)
+
+age = 34
+print("You are " + str(age))
