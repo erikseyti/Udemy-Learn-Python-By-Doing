60ce1a64e685834ce3fad9d76cd07a032d4a490f
Erik Seyti <erikseyti@gmail.com>
2021-03-02 23:57:18 -0300

    Create example of using input by the user

---
+++ Section 1 - Intro To Python/get_data_from_user.py
@@ -0,0 +1,10 @@
+my_name = "Erik"
+# get input from the terminal from user
+# returns in a string format
+your_name = input("Enter your name: ")
+
+print(f"Hello {your_name}. My name is {my_name}")
+
+age = input("Enter your age:")
+months = int(age) * 12
+print(f"You have lived for {months} months.")
