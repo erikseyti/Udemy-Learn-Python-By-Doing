3f0e5a34c83b63ca159baa9cdc7665d5663ee432
Erik Seyti <erikseyti@gmail.com>
2021-03-12 18:01:29 -0300

    Add example of Dictionary

---
+++ Section 1 - Intro To Python/dictionaries.py
@@ -0,0 +1,22 @@
+friend_ages = {"Rolf":24, "Adam": 30, "Anne": 27}
+
+print(f"Rolf's Age is {friend_ages['Rolf']}")
+
+# add Value on dicionary
+friend_ages["Bob"] = 20
+friend_ages["Rolf"] = 25
+print(f"Friend's dictionary age: {friend_ages}")
+
+friends = (
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wolfe", "age": 30},
+    {"name": "Anne Pun", "age": 27}
+)
+
+print(friends[0]["name"])
+print(friends[1]["name"])
+print(friends[2]["name"])
+
+friends = [("Rolf", 24), ("Adam", 30), ("Anne", 27)]
+friends_ages = dict(friends)
+print(friend_ages)
