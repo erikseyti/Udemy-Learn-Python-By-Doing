c7a1dbb411bca94490609c433020caae2d3cceb6
Erik Seyti <erikseyti@gmail.com>
2021-03-23 21:36:04 -0300

    Criado exemplos de for loops em Python
    Signed-off-by: Erik Seyti <erikseyti@gmail.com>

---
+++ Section 2 - Python Fundamentals/for_loops.py
@@ -0,0 +1,12 @@
+friends = ["Rolf", "Jen", "Anne"]
+
+for friend in friends:
+    print(friend)
+print("")
+print("-----")
+print("")
+elements = [0,1,2,3,4,5,6,7,8,9]
+
+for index in elements:
+    print(index)
+    print("Hello Word!")
---
+++ Section 2 - Python Fundamentals/for_loops2.py
@@ -0,0 +1,14 @@
+for index in range(10):
+    print(index)
+
+print("")
+print("-----")
+
+for index in range(5, 10):
+    print(index)
+
+print("")
+print("-----")
+
+for index in range(2, 20, 3):
+    print(index)
