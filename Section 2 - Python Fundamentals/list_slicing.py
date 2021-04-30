e209e948958678452d0efdb932d9bb540d6aaaa0
Erik Seyti <erikseyti@gmail.com>
2021-04-30 19:24:53 -0300

    Example of List Slicing

---
+++ Section 2 - Python Fundamentals/list_slicing.py
@@ -0,0 +1,18 @@
+friends = ["Rolf", "Charlie", "Anna", "Bob", "Jen"]
+
+print(friends[2:4])
+
+# all Friends but the first one.
+print(friends[1:])
+
+# get all friends before the element number 5 and after
+print(friends[:4])
+
+# create a new list with the same elements
+print(friends[:])
+
+# get all 3 last elements
+print(friends[-3:])
+
+# get all elements except the last 2
+print(friends[:-2])
